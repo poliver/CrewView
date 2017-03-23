a86a40084c020f81ce449564bdfb28997545e42b
Phil Oliver <sortofrican90@gmail.com>
2017-03-22 20:51:10 -0700

    Add initial project shell

---
+++ app/src/main/java/xyz/philoliver/crewview/UserListActivity.java
@@ -0,0 +1,13 @@
+package xyz.philoliver.crewview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_list);
+    }
+}
